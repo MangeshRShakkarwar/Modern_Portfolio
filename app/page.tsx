18ceab1d029bf2ad1baf27519f62fbe88624e3a2
MangeshRShakkarwar <mangeshramraoshakkarwar@gmail.com>
2024-05-22 11:40:00 +0530

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,25 @@
-"use client";
-
+"use client"
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import { Progress } from "@/components/Preloader"; // Ensure the path is correct
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 import { useEffect, useState } from 'react';
 
 const Home = () => {
-  const [progress, setProgress] = useState(0);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    // Simulate a loading process
-    const interval = setInterval(() => {
-      setProgress((prev) => {
-        if (prev >= 100) {
-          clearInterval(interval);
-          return 100;
-        }
-        return prev + 10;
-      });
-    }, 300); // Adjust the interval as needed
-
-    return () => clearInterval(interval);
+    setIsClient(true);
   }, []);
 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      {progress < 100 ? (
-        <Progress value={progress} className="my-custom-class" />
-      ) : (
+      {isClient && (
         <div className="max-w-7xl w-full">
           <FloatingNav navItems={navItems} />
           <Hero />
